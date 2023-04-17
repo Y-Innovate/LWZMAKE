9fbca4a89cbc44b39d26a374c149e62fa516dc7d
YBTKS <bobby.tjassens.keiser@y-innovate-it.com>
2023-04-17 13:12:11 +0000

    Removed old DOCU

--- DOCU/scripts.js
+++
@@ -1,9 +0,0 @@
-function NumberLines(element, nr) {
-	var content = "";
-
-	for (i = 1; i <= nr; i++) {
-		content = content + "<span class=\"clsLineNumber\">" + ("0" + i).slice(-2) + "|</span>\n";
-	}
-
-	element.innerHTML = content;
-}
